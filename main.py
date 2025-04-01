4df3af2694f2d05d344477b70cc97e859751172b
Samuel Rocha <41922351+Samtlokomemo@users.noreply.github.com>
2025-04-01 08:23:04 -0300

    FLASK

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from flask import Flask, jsonify, request
 
+app = Flask(__name__)
+
+# Rota inicial
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# Rota para visualizar algoritmos
+@app.route('/algorithms', methods=['GET'])
+def get_algorithms():
+    # Esta rota retornará os algoritmos disponíveis
+    algorithms = ["BST", "AVL", "B-Tree", "Fibonacci Tree", "Perfect Hashing", "Universal Hashing", "Skip List"]
+    return jsonify(algorithms)
+
+if __name__ == '__main__':
+    app.run(debug=True)
